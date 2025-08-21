69acb2f63864f9d2e93192eb64d88a61968eb5f2
azryl <azryl.career@gmail.com>
2025-08-21 17:05:06 +0800

    Write a function that loops through the array and displays each book on the page

--- script.js
+++ script.js
@@ -19,7 +19,41 @@ function addBookToLibrary(title, author, pages, hasRead) {
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Diary of a Young Girl", "Anne Frank", 256, false);
+addBookToLibrary("Romeo and Juliet", "William Shakespeare", 301, false);
 
-console.log("Book ID:", myLibrary[0].id);
-console.log(myLibrary[0].info()); 
+for (const book of myLibrary) {
+  const cardContainer = document.querySelector(".card-container");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const title = document.createElement("h2");
+  title.textContent = book.title
+  title.classList.add("card-title");
+
+  const author = document.createElement("p");
+  author.textContent = "Author: " + book.author;
+  author.classList.add("card-content");
+  author.classList.add("book-author");
+
+  const pages = document.createElement("p");
+  pages.textContent = "Pages: " + book.pages;
+  pages.classList.add("card-content");
+  pages.classList.add("book-pages");
+
+  const status = document.createElement("p");
+  status.textContent = "Status: " + book.hasRead;
+  status.classList.add("card-content");
+  status.classList.add("book-status");
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(status);
+
+  cardContainer.appendChild(card);
+}
+
+
 
